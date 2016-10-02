937a5d43dc86054fed7ec06f8bf8837daa08f2a4
samilaaroussi <sami@slt.fr>
2016-10-02 11:15:53 +0800

    to_tsv

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -2,13 +2,58 @@
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
+  attr_accessor :data
+
   def take_tsv(tsv)
+     tsv_arr, data = [], []
 
-  end
+     # Per row
+     r = tsv.split("\n");
 
-  # to_tsv: converts @data into tsv string
-  # returns: String in TSV format
-  def to_tsv
+     r.each do |e|
+       tsv_arr.push(e.split("\t"));
+     end
 
-  end
-end
+     field = tsv_arr.shift
+
+     # Transfer tsv into field => value
+     tsv_arr.each do |row|
+         hash = {}
+
+         row.each_with_index do |conlumn, i|
+             hash[field[i]] = conlumn
+         end
+
+         data.push(hash)
+     end
+
+     @data = data
+   end
+
+   # to_tsv: converts @data into tsv string
+   # returns: String in TSV format
+   def to_tsv
+     yaml = @data
+     separator = {:tab => "\t", :newline => "\n"}
+     field = []
+
+     yaml[0].each do |k, v|
+         field.push(k)
+     end
+
+     data = [field.join(separator[:tab])]
+
+     yaml.each do |e|
+         str = []
+
+         e.each do |k, v|
+             str.push(v)
+         end
+
+         data.push(str.join(separator[:tab]))
+     end
+
+     data.push('').join(separator[:newline])
+   end
+
+ end
